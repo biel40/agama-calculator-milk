7670b8e9f55b18f412a15e4a2c0f5f1b6c7d4db6
Gabriel Borrás Serra <biel40@MacBook-Air-de-Gabriel.local>
2025-01-02 15:32:05 +0100

    finished stetic adjustments to web app calculator agama

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
 export const routes: Routes = [
     { path: '', component: AuthComponent },
-    { path: 'dashboard', component: DashboardComponent }, 
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'calculator', component: CalculatorComponent }
 ];
--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -2,20 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../modules/material.module';
 import { SupabaseService } from '../../services/supabase/supabase.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
+    selector: 'app-calculator',
+    templateUrl: './calculator.component.html',
+    styleUrls: ['./calculator.component.scss'],
     imports: [
-        MaterialModule
+        MaterialModule,
+        FormsModule
     ],
     standalone: true,
 })
-export class DashboardComponent implements OnInit {
+export class CalculatorComponent implements OnInit {
 
     // Usar modelos para definir el tipo de datos
     public profile: any;
+
+    public volume1: number = 0;
+    public volume2: number = 0;
+    public fat1: number = 0;
+    public fat2: number = 0;
+
+    public totalFatCalculated: number = 0;
+    public totalVolumeCalculated: number = 0;
 
     constructor(
         private _supabaseService: SupabaseService,
@@ -40,10 +50,6 @@ export class DashboardComponent implements OnInit {
         }
     }
 
-    public openCalculator(): void {
-        this._router.navigate(['calculator']);
-    }
-
     public async signOut(): Promise<void> {
         try {
             let response = await this._supabaseService.signOut();
@@ -54,6 +60,25 @@ export class DashboardComponent implements OnInit {
         } catch(error) {
             console.error(error);
         }
+    }
+
+    public calculate(): void {
+        alert('Calculando...');
+
+        // Primero calcularemos la Grasa Final. 
+        // La grasa final se calcula .....
+
+        // Una vez que tengamos la grasa final, calculamos el total del volumen del producto
+        let totalVolume = this.volume1 + this.volume2;
+        let totalFat = (this.volume1 * this.fat1 + this.volume2 * this.fat2) / totalVolume;
+
+        // Aplicar bien los calculos
+        this.totalFatCalculated = totalFat;
+        this.totalVolumeCalculated = totalVolume;
+    }
+
+    public goBack(): void {
+        this._router.navigate(['/dashboard']);
     }
 
 }
